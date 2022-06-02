69d6eec08314b9c5ef8fec8fc3d13b0fea8546ae
katzy687 <katzy687@gmail.com>
2022-06-02 15:44:19 +0300

    docker busybox

---
+++ docker_busybox.sh
@@ -0,0 +1,6 @@
+# https://docker-curriculum.com/
+# test docker pull and run with busybox container
+docker pull busybox
+docker run busybox
+docker run busybox echo "hello from busybox"
+docker ps -a
