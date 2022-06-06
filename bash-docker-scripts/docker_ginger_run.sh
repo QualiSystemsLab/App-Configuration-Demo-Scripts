aa9a363804fdff6b79231930c878a7f8072fea3d
katzy687 <katzy687@gmail.com>
2022-06-06 23:25:59 +0300

    fix docker images command

--- bash-docker-scripts/docker_ginger_run.sh
+++ bash-docker-scripts/docker_ginger_run.sh
@@ -2,7 +2,7 @@ echo "pulling latest ginger container..."
 sudo docker pull ghcr.io/ginger-automation/gingerexecutionservice:latest
 
 echo "=== docker images ==="
-sudo docker images ls -a
+sudo docker images -a
 
 echo "running ginger container - registering to handler url: $EXECUTION_HANDLER_URL"
 # sudo docker run ghcr.io/ginger-automation/gingerexecutionservice:latest --AgentConfigurations:HandlerURL=$EXECUTION_HANDLER_URL --AgentConfigurations:Name=DockerAgent1 --AgentConfigurations:TagName=DockerAgent --AgentConfigurations:ProcessesCapacity=2 --AgentConfigurations:DisableHandlerIpFilter=True
