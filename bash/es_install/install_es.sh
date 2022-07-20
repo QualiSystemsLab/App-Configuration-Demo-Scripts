c7217de64912ac55db1f2ebd6a82034e540dfd2d
katzy687 <katzy687@gmail.com>
2022-07-20 13:00:36 +0300

    update ansible flow

--- bash/es_install/install_es.sh
+++ bash/es_install/install_es.sh
@@ -1,3 +1,14 @@
-wget $env:SCRIPT_URL
-chmod +x cloudshell_es_install_script.sh
-./cloudshell_es_install_script $env:CS_HOST $env:CS_USER $env:CS_PASSWORD $env:ES_NAME
+sudo wget $env:SCRIPT_URL
+sudo chmod +x cloudshell_es_install_script.sh
+
+echo "starting es install"
+sudo ./cloudshell_es_install_script $env:CS_HOST $env:CS_USER $env:CS_PASSWORD $env:ES_NAME
+
+echo "install ansible"
+sudo python3 -m pip install ansible
+
+echo "install pywinrm"
+sudo python3 -m pip install pywinrm
+
+echo "install ssh pass"
+sudo yum install sshpass
